9af79caf43a9c496cf303876538409ea4ea7a516
Victor Martins <martinsvictor287@gmail.com>
2023-08-25 23:52:33 +0300

    add state management

---
+++ src/state-management/contexts/taskContext.ts
@@ -0,0 +1,15 @@
+import { Task, TaskAction } from '../reducers/tasksReducer';
+
+import { Dispatch } from 'react';
+import React from 'react';
+
+interface TasksContextType {
+	tasks: Task[];
+	dispatch: Dispatch<TaskAction>;
+}
+
+const TasksContext = React.createContext<TasksContextType>(
+	{} as TasksContextType
+);
+
+export default TasksContext;
